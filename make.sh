9323b2fc5064669acdd4dab4bef26d2d37557ead
Chris West (Faux) <git@goeswhere.com>
2013-01-02 15:22:49 +0000

    reformat make.sh

--- make.sh
+++ make.sh
@@ -1,1 +1,4 @@
-(echo '<kanjidic2>'; zcat kanjidic2.xml.gz | sed '1,/^<kanjid/d') | xqilla -i /dev/stdin make.xqy | gzip --best > datums.json.gz
+#!/bin/sh
+(echo '<kanjidic2>'; zcat kanjidic2.xml.gz | sed '1,/^<kanjid/d') |
+	xqilla -i /dev/stdin make.xqy |
+	gzip --best > datums.json.gz
